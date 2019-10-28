d04763ff270ab5827eff264657de028cafdb1e9d
Tony Mullen <tony.mullen@cdl.co.uk>
2019-10-28 14:19:26 +0000

    change of name of the install schema script

--- runCreateFastRefreshDataSchemas.sh
+++ runCreateFastRefreshDataSchemas.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#File: create-fastrefresh-data-schemas.sh
+#File: runCreateFastRefreshDataSchemas.sh
 #Desc:
 #
 # Amendment History:
@@ -9,7 +9,7 @@
 
 
 . ./module_set_variables.sh
-export LOG_FILE=/tmp/create-fastrefresh-data-schemas_install_`date +%Y%m%d-%H%M`.log
+export LOG_FILE=/tmp/runCreateFastRefreshDataSchemas_install_`date +%Y%m%d-%H%M`.log
 echo "INFO: Set variables" >> $LOG_FILE
 echo "INFO: LOG_FILE parameter set to $LOG_FILE" >> $LOG_FILE
 echo "INFO: MODULEOWNER parameter set to $MODULEOWNER" >> $LOG_FILE
