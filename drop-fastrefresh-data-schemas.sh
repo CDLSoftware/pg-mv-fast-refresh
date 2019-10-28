a8366ce30e137043eb6f97153dc8eaf3943c0823
Tony Mullen <tony.mullen@cdl.co.uk>
2019-10-28 11:32:09 +0000

    new script drop-fastrefresh-data-schemas.sh

---
+++ drop-fastrefresh-data-schemas.sh
@@ -0,0 +1,106 @@
+#! /bin/bash
+#File: drop-fastrefresh-data-schemas.sh
+#Desc:
+#
+# Amendment History:
+# Date:      Who:       Desc:
+# 28/10/19   T.Mullen   Initial;
+#
+
+
+. ./module_set_variables.sh
+export LOG_FILE=/tmp/drop-fastrefresh-data-schemas_install_`date +%Y%m%d-%H%M`.log
+echo "INFO: Set variables" >> $LOG_FILE
+echo "INFO: LOG_FILE parameter set to $LOG_FILE" >> $LOG_FILE
+echo "INFO: MODULEOWNER parameter set to $MODULEOWNER" >> $LOG_FILE
+echo "INFO: PGUSERNAME parameter set to $PGUSERNAME" >> $LOG_FILE
+echo "INFO: SOURCEUSERNAME parameter set to $SOURCEUSERNAME" >> $LOG_FILE
+echo "INFO: MVUSERNAME parameter set to $PGUSERNAME" >> $LOG_FILE
+echo "INFO: HOSTNAME parameter set to $HOSTNAME" >> $LOG_FILE
+echo "INFO: PORT parameter set to $PORT" >> $LOG_FILE
+echo "INFO: DBNAME parameter set to $DBNAME" >> $LOG_FILE
+echo "INFO: MODULE_HOME parameter set to $MODULE_HOME" >> $LOG_FILE
+
+PGPASS=$PGPASSWORD
+
+function dropmvschema
+{
+
+echo "INFO: Dropping MV user $MVUSERNAME " >> $LOG_FILE
+
+PGPASSWORD=$PGPASS
+
+
+psql --host=$HOSTNAME --port=$PORT --username=$PGUSERNAME --dbname=$DBNAME << EOF3 >> $LOG_FILE 2>&1
+
+ REVOKE ALL PRIVILEGES ON DATABASE "$DBNAME" from $MVUSERNAME;
+ GRANT ALL PRIVILEGES ON SCHEMA $MVUSERNAME to $PGUSERNAME;
+ ALTER SCHEMA $MVUSERNAME OWNER TO $PGUSERNAME;
+ DROP SCHEMA $MVUSERNAME CASCADE;
+ REVOKE $SOURCEUSERNAME from $MVUSERNAME;
+ revoke pgmv$_role from $MVUSERNAME;
+ revoke $SOURCEUSERNAME from $MODULEOWNER;
+ revoke $MVUSERNAME  from $MODULEOWNER;
+ revoke $MODULEOWNER from $MVUSERNAME ;
+ revoke $MVUSERNAME from $PGUSERNAME;
+ REVOKE ALL PRIVILEGES ON SCHEMA $MVUSERNAME from $MVUSERNAME;
+ revoke USAGE ON SCHEMA $MODULEOWNER from $MVUSERNAME ;
+ DROP USER $MVUSERNAME;
+
+EOF3
+}
+
+function dropsourceschema
+{
+echo "INFO: Dropping Schema user $SCHEMAUSERNAME " >> $LOG_FILE
+
+PGPASSWORD=$PGPASS
+
+
+psql --host=$HOSTNAME --port=$PORT --username=$PGUSERNAME --dbname=$DBNAME << EOF3 >> $LOG_FILE 2>&1
+
+ REVOKE ALL PRIVILEGES ON DATABASE "$DBNAME" from $SOURCEUSERNAME;
+ GRANT ALL PRIVILEGES ON SCHEMA $SOURCEUSERNAME to $PGUSERNAME;
+ ALTER SCHEMA $SOURCEUSERNAME OWNER TO $PGUSERNAME;
+ DROP SCHEMA $SOURCEUSERNAME CASCADE;
+ REVOKE $SOURCEUSERNAME from $PGUSERNAME;
+ REVOKE $SOURCEUSERNAME FROM $MODULEOWNER;
+ REVOKE USAGE ON SCHEMA $SOURCEUSERNAME FROM $MODULEOWNER;
+ REVOKE ALL ON SCHEMA $MODULEOWNER FROM $SOURCEUSERNAME;
+ DROP USER $SOURCEUSERNAME;
+
+
+EOF3
+}
+
+
+function truncatemoduletbls
+{
+echo "INFO: Truncating modules tables" >> $LOG_FILE
+
+PGPASSWORD=$MODULEOWNERPASS
+
+
+psql --host=$HOSTNAME --port=$PORT --username=$MODULEOWNER --dbname=$DBNAME << EOF4 >> $LOG_FILE 2>&1
+
+truncate table pgmview_logs;
+truncate table pgmviews;
+truncate table pgmview_oj_details;
+
+
+EOF4
+}
+
+
+read -p "Are you sure you want to remove the schemas - $MVUSERNAME & $SCHEMAUSERNAME (y/n)?" choice
+case "$choice" in
+  y|Y ) echo "yes the schemas - - $MVUSERNAME & $SCHEMAUSERNAME will be dropped"
+        dropmvschema
+        dropsourceschema
+        truncatemoduletbls;;
+  n|N ) echo "no exiting";;
+  * ) echo "invalid choice exiting";;
+esac
+
+
+echo "INFO: Drop Complete check logfile for status - $LOG_FILE"
