fb1341d37d561551a24d61eea0733648fd989fe3
David Day <David.Day@cdl.co.uk>
2020-07-01 14:26:28 +0100

    defect fix to stop DML Type INSERT duplicates when row already exists

--- BuildScripts/mvComplexFunctions.sql
+++ BuildScripts/mvComplexFunctions.sql
@@ -707,7 +707,7 @@ BEGIN
 			pInnerJoinTableAliasArray,
 			pInnerJoinTableRowidArray,			
 			pInnerJoinOtherTableNameArray,		
-			pInnerJoinOtherTableAliasArray.
+			pInnerJoinOtherTableAliasArray,
 			pInnerJoinOtherTableRowidArray
     );
 
